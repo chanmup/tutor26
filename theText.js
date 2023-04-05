b824e20dc9f745b7ea3ca021318f8b390143c90a
chanmup <126180165+chanmup@users.noreply.github.com>
2023-04-05 00:17:09 -0700

    Delete theText.js

--- theText.js
+++
@@ -1,1 +0,0 @@
-//<![CDATA[function replaceText(){if(!document.getElementById){return;}bodyText = document.getElementById("Blog1");theText = bodyText.innerHTML;theText = theText.replace(/s72-c/gi, "s200-c");bodyText.innerHTML = theText;}replaceText();//]]>
